59e18b66cdb323f13c56d3ec7945eeacecf77f20
Tolmax <tolmax2000@mail.ru>
2023-07-28 00:27:33 +0300

    First revue

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,10 @@
 const popupOpenButtonElement = document.querySelector('.profile__open-popup');
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = document.querySelector('.popup__close');
-const popupSubmitButtonElement = document.querySelector('.popup__submit');
 
 const formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__job');
+let nameInput = document.querySelector('.popup__input_text_name');
+let jobInput = document.querySelector('.popup__input_text_about');
 
 let profName = document.querySelector('.profile__name');
 let profJobtitle = document.querySelector('.profile__job-title');
@@ -13,23 +12,20 @@ let profJobtitle = document.querySelector('.profile__job-title');
 function openPopup() {
     nameInput.value = profName.textContent;
     jobInput.value = profJobtitle.textContent;
-    console.log('OPEN');
     popupElement.classList.add('popup_opened');
 }       
 popupOpenButtonElement.addEventListener('click', openPopup)
 
 function closePopup() {
-    console.log('CLOSE');
     popupElement.classList.remove('popup_opened');
 }
 popupCloseButtonElement.addEventListener('click', closePopup);
-popupSubmitButtonElement.addEventListener('click', closePopup);
 
 function handleFormSubmit(evt) {
     profName.textContent = nameInput.value;
     profJobtitle.textContent = jobInput.value;
+    closePopup();
     evt.preventDefault();
   }
 formElement.addEventListener('submit', handleFormSubmit);
-//popupSubmitButtonElement.addEventListener('submit', handleFormSubmit);
 
